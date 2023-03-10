2f75cbd815ee2151e70ee7384c8d57a1f5ae357e
XSbzi_Uh51 <XSbzi_Uh51@STHQ0124-09>
2023-03-10 21:19:43 +0400

    Add project files.

---
+++ Storeee/Add.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Store
+{
+    /// <summary>
+    /// Interaction logic for Add.xaml
+    /// </summary>
+    public partial class Add : Window
+    {
+        public Add()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void btnn_Click(object sender, RoutedEventArgs e)
+        {
+            int count;
+            count = int.Parse(counter.Text);
+            DialogResult = true;
+        }
+    }
+}
---
+++ Storeee/Model/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Model
+{
+    public class Product : INotifyPropertyChanged
+    {
+        private double price;
+        private string? name;
+        public int Id { get; set; }
+        public string? Name { get => name; set { name = value; RaisePropertyChanged(); } }
+        public double Price { get => price; set { price = value; RaisePropertyChanged(); } }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
